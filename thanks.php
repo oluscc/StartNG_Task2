b5867286ab624b5d36561878f78d206e6338720d
oluscc <olusegun@supreme-concepts.com>
2019-09-08 01:04:50 +0100

    Contact Form now saves messages into a PHP File

---
+++ thanks.php
@@ -0,0 +1,18 @@
+<?php
+ $file_path = 'contact_form.txt';
+ if (isset($_POST['fullName']) && isset($_POST['email']) && isset($_POST['title']) && isset($_POST['message'])) {
+    $file_handler = fopen($file_path,"a+");
+    $form_content = '<< '. $_POST['fullName'].' | '.$_POST['email'].' | '.$_POST['title'].' | '.$_POST['message']. ' >> ...';
+    fwrite($file_handler, $form_content); // Write information to the file
+    fclose($file_handler); // Close the file
+ }
+
+ echo '
+        <script>
+        alert("Thank You For Contacting Me");    
+        //window.location.replace("https://oluscc.github.io/StartNG_Tasks");
+        window.location.replace("index.html");
+        </script>
+    ';
+
+?>
